cb5a85f96abac6b0e57d160777ca94614ca40683
Michael Gmelin <freebsd@grem.de>
2022-10-19 11:58:49 +0200

    Try to run CI scripts on github

--- tests/CI/run.sh
+++ tests/CI/run.sh
@@ -523,7 +523,7 @@ BROKEN_FLV
 }
 
 STACKS="ipv4 dual ipv6"
-VERSIONS="12.2 11.4"
+VERSIONS="12.3 13.1"
 TYPES="single multi"
 #NETWORKS="alias inherit public-bridge private-bridge"
 NETWORKS="inherit public-bridge private-bridge"
